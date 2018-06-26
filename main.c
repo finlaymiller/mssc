6f7c12e22183b876847542dc52e565ba91af8199
Finlay Miller <finlay@dal.ca>
2018-06-25 21:39:34 -0300

    Added multithreading

--- main.c
+++ main.c
@@ -15,19 +15,22 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <errno.h>
 #include <string.h>
 #include <pthread.h>
 #include <ctype.h>
 
 #define THREADCOUNT 11
 
-int read_grid(int (*grid)[9], int FILE *input);
+int read_grid(int (*grid)[9], char *);
+void *check_subgrid(void *);
+void *check_row(void *);
+void *check_col(void *);
 
 typedef struct
 {
     int row;
     int col;
+    int *grid[9];
 } parameters;
 
 int main(int argc, char *argv[])
@@ -38,17 +41,15 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    int grid[9][9];
-
-    FILE *input = fopen(argv[1], "r");
+    int grid[9][9], threadStatus[THREADCOUNT], check = 0;
 
     parameters *data[9];
     int i = 0;
 
     // create grid object
-    for (int col = 0; col < 9; col += 3)
-    {
-        for (int row = 0; row < 9; row += 3)
+    for (int row = 0; row < 9; row += 3)
+    {   
+        for (int col = 0; col < 9; col += 3)
         {
             // create grid
             data[i] = (parameters *) malloc(sizeof(parameters));
@@ -65,11 +66,80 @@ int main(int argc, char *argv[])
         }
     }
 
-    // validate grid
+    // fill grid object
+    if(read_grid(grid, argv[1]))
+    {
+        printf("Failed to read grid.\n");
+        return 1;
+    }
+
     pthread_t tid[THREADCOUNT];
+
+    // create subgrid check thread
+    for(int g = 0; g < 9; g++)
+    {
+        if(pthread_create(&tid[g], NULL, check_subgrid, (void *)data[0]))
+            printf("Error creating subgrid check thread.\n");
+    }
+
+    // create row check thread
+    if(pthread_create(&tid[9], NULL, check_row, (void *)data[0]))
+        printf("Error creating row check thread.\n");
+
+    // create column check thread
+    if(pthread_create(&tid[10], NULL, check_col, (void *)data[0]))
+        printf("Error creating column check thread.\n");
+
+    // check return status of each thread
+    for (int t = 0; t < THREADCOUNT; t++)
+    {
+        if(threadStatus[t])
+        {
+            check = 1;
+            break;
+        }
+    }
     
+    if (check)
+    {
+        printf("Sudoku Solution Invalid.\n");
+    }
+    else
+    {
+        printf("Sudoku Solution Valid.\n");
+    }
+    
+
 }
 
+/*
+    PURPOSE: checks validity of sudoku subgrid
+    INPUT:  d       starting coordinates of subgrid to check
+    OUTPUT: 1       if success
+            0       if failure
+*/
+void *check_subgrid(void * d)
+{
+    int foundDigits[10] = {0};
+    parameters *params = (parameters *) d;
+
+    // check each value in subgrid
+    for (int r = params->row; r < params->row + 3; r++)
+    {
+        for (int c = params->col; c < params->col + 3; c++)
+        {
+            // check to see if the current digit has been found before
+            if (foundDigits[params->grid[r][c]] == 1)
+                // invalid subgrid
+                return (void *) 0;
+            
+            // add it if it hasn't 
+            foundDigits[params->grid[r][c]] = 1;
+        }
+    }
+    // valid subgrid
+    return 1;
+}
 
 /*
     PURPOSE: Reads sudoku grid from file
@@ -78,12 +148,13 @@ int main(int argc, char *argv[])
     OUTPUT: 0       if success
             1       if failure
 */
-int read_grid(int (*grid)[9], int FILE *input)
+int read_grid(int (*grid)[9], char *file)
 {
     char val;
     int r, c, valCount;
+    FILE *f = fopen(file, "r");
 
-    while(fread(&val, 1, 1, input) > 0 && valCount < 81)
+    while(fread(&val, 1, 1, f) > 0 && valCount < 81)
     {
         if (val != '\n')
         {
@@ -107,4 +178,95 @@ int read_grid(int (*grid)[9], int FILE *input)
     }
 
     return 0;
+}
+
+/*
+    PURPOSE: checks validity of sudoku subgrid
+    INPUT:  d       starting coordinates of subgrid to check
+    OUTPUT: 1       if success
+            0       if failure
+*/
+void *check_subgrid(void *d)
+{
+    int foundDigits[10] = {0};
+    parameters *params = (parameters *) d;
+
+    // check each value in subgrid
+    for (int r = params->row; r < params->row + 3; r++)
+    {
+        for (int c = params->col; c < params->col + 3; c++)
+        {
+            // check to see if the current digit has been found before
+            if (foundDigits[params->grid[r][c]] == 1)
+                // invalid subgrid
+                return (void *) 0;
+            
+            // add it if it hasn't 
+            foundDigits[params->grid[r][c]] = 1;
+        }
+    }
+    // valid subgrid
+    return 1;
+}
+
+/*
+    PURPOSE: checks validity of sudoku row
+    INPUT:  d       starting coordinates of row to check
+    OUTPUT: 1       if success
+            0       if failure
+*/
+void *check_row(void *d)
+{
+    int foundDigits[10] = {0};
+    parameters *params = (parameters *) d;
+
+    // check each value in subgrid
+    for (int r = 0; r < 9; r++)
+    {
+        for (int c = 0; c < 9; c++)
+        {
+            // check to see if the current digit has been found before
+            if (foundDigits[params->grid[r][c]] == 1)
+                // invalid subgrid
+                return (void *) 0;
+            
+            // add it if it hasn't 
+            foundDigits[params->grid[r][c]] = 1;
+        }
+        // reset check array for next row
+        memset(foundDigits, 0, sizeof(int)*10);
+    }
+    // valid subgrid
+    return 1;
+}
+
+/*
+    PURPOSE: checks validity of sudoku column
+    INPUT:  d       starting coordinates of column to check
+    OUTPUT: 1       if success
+            0       if failure
+*/
+void *check_col(void *d)
+{
+    int foundDigits[10] = {0};
+    parameters *params = (parameters *) d;
+
+    // check each value in subgrid
+    for (int c = 0; c < 9; c++)
+    {
+        for (int r = 0; r < 9; r++)
+        {
+            // check to see if the current digit has been found before
+            if (foundDigits[params->grid[r][c]] == 1)
+                // invalid subgrid
+                return (void *) 0;
+            
+            // add it if it hasn't 
+            foundDigits[params->grid[r][c]] = 1;
+        }
+        // reset check array for next row
+        memset(foundDigits, 0, sizeof(int)*10);
+    }
+    // valid subgrid
+    return 1;
 }
